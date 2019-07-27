0f32c0cde0201e3446552a8218e9b72b9ea95658
Marzena <kacper_092@o2.pl>
2019-07-27 12:29:46 +0200

    Initial commit

---
+++ src/main/java/pl/bebel/springbootapp/AnimalFactClient.java
@@ -0,0 +1,25 @@
+package pl.bebel.springbootapp;
+
+
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.client.RestTemplate;
+
+@Controller
+public class AnimalFactClient {
+    public AnimalFact[] getAnimalFact() {
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<AnimalFact[]> exchange = restTemplate.exchange("https://cat-fact.herokuapp.com/facts/random?animal_type=dog&amount=4",
+                HttpMethod.GET,
+                HttpEntity.EMPTY,
+                AnimalFact[].class
+        );
+        return exchange.getBody();
+
+    }
+
+
+
+}
